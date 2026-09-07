2fb07337f808649ba44448c99006b03728ff0c59
Yunus Said <yunussaid2704@gmail.com>
2026-09-07 19:26:39 +0800

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Root Endpoint: API metadata
 app.get('/', (req, res) => {
-    res.status(200).send('Hello, server');
+    res.status(200).json({
+        name: "Task API",
+        version: "1.0",
+        endpoints: ["/tasks"]
+    });
+});
+
+//Health Check Endpoint
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: "ok"
+    });
 });
 
 app.listen(port, () => {
